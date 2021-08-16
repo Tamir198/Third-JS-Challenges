52c035406868063aa568274733cdbceba1fa3af7
Tamir <1tamir198@gmail.com>
2021-08-16 15:21:41 +0300

    Adding search functionallity

--- CompleteInput/index.js
+++ CompleteInput/index.js
@@ -1,12 +1,47 @@
-
 const api = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-/* 
-  In order to find a pattern in a string you will mostly want to use regex and string.match method.
-  You can create a new regex with a custom string by using the RegExp constructor and apply to it the g an i flags,
-  g for global and i in order to ignore case. 
-  Example :
-    const string = '#$)($#)(hey)#$)(@02)';
-    const patternToFind = new RegExp('hey','gi');
-    return string.match(patternToFine) // true;
-*/
+const cities = getplaces();
+var inputSearchBox = document.querySelector(".place-input");
+var listOfCities = document.querySelector(".place-list");
+
+function getplaces() {
+  let citiesArr = [];
+  fetch(api)
+    .then(res => res.json())
+    .then(data => data.forEach(city => citiesArr.push(city)));
+  console.log(citiesArr);
+  return citiesArr;
+}
+
+
+inputSearchBox.addEventListener('input', () => {
+  let conentOfSearchBox = inputSearchBox.value;
+  //Got Value
+  if (conentOfSearchBox) {
+    listOfCities.innerHTML = '';
+    let filteredCities = updateListOfCities(getMatchingCitiesArr(conentOfSearchBox));
+    console.log(filteredCities);
+  }
+  else
+    listOfCities.innerHTML = "<li>Filter for A city</li><li>or a state</li>";
+
+})
+
+function updateListOfCities(citiesArr){
+  citiesArr.map(city =>{
+    listOfCities.insertAdjacentHTML('beforeend', `<li>${city}</li>`); 
+  })
+  return;
+}
+
+function getMatchingCitiesArr(text) {
+  let mathingCities = [];
+  var regex = new RegExp(text, 'g');
+  cities.map(item => {
+    if (item.city.match(regex) || item.state.match(regex)) {
+      console.log(`${item.city}, ${item.state}`);
+      mathingCities.push(`${item.city}, ${item.state}`);
+    }
+  })
+  return mathingCities;
+}
